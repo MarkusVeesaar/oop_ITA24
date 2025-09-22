ea03ea24b50bbae966a17e57f2ae456a0edea810
Markus <Markus.veesaar@voco.ee>
2025-09-22 14:47:25 +0300

    Add BMI calculting to Person

--- 1_class_and_object/person.js
+++ 1_class_and_object/person.js
@@ -1,6 +1,36 @@
 class  Person {
+    #height = 0
+    #weight = 0
     #name = ""
     #age = 0
+    printperson(name){
+        console.log(this.#name + " age " + this.#age +" years"+ " height: "+ this.height+"m weight: "+ this.weight + "kg")
+    }
+    getBMI(){
+        let number = Number(this.weight) / (Number(this.height)*Number(this.height))
+        return number.toFixed(2)
+    }
+    getheight(){
+        return this.height
+    }
+    setheight(height){
+        this.height = height
+    }
+    gainheight(){
+        this.height++
+    }
+    getweight(){
+        return this.weight
+    }
+    setweight(weight){
+        this.weight = weight
+    }
+    grow(){
+        this.height++
+    }
+    getolder(){
+        this.#age++
+    }
     setName(name){
         this.#name = name
     }
@@ -10,7 +40,6 @@ class  Person {
     setAge(age){
         this.#age = age
     }
-
     getAge(){
         return this.#age
     }
--- 1_class_and_object/persons.js
+++ 1_class_and_object/persons.js
@@ -2,4 +2,28 @@
 
 const Person = require(`./person.js`)
 const bob = new Person()
-console.log(bob.age)
+const brian = new Person()
+const Alice = new Person()
+
+while (bob.getAge() < 15){
+    bob.getolder()
+};
+
+bob.setName("bob");
+brian.setName("brian")
+Alice.setName("Alice")
+
+bob.setheight(1.70)
+brian.setheight(1.75)
+Alice.setheight(1.80)
+
+bob.setweight("100")
+brian.setweight("90")
+Alice.setweight("80")
+
+console.log(bob.getBMI())
+
+bob.printperson();
+brian.printperson();
+Alice.printperson();
+
